030d03f9de23a5177ebe4c01417b8a9910f1c74c
Andres Clavijo Alvarez <andresclavijoalvarez@gmail.com>
2021-12-06 11:45:03 -0500

    Revert "Inicial"
    This reverts commit a2de5421ad6af2263b4dcc1e93579c942f59177f.

--- conexion.php
+++
@@ -1,4 +0,0 @@
-<?php
-$conexion = mysql_connect("localhost", "root", "crisalltex") or die ('Se ha presentado un problema al conectar con la base de datos. Error:' . mysql_error());
-mysql_select_db("encuesta", $conexion);
-?>
